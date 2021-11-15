4b71b0a1e4039d3f70dba1e7067e2bead815d4d1
Jhonatan Teixeira <jhonatan.teixeira@gmail.com>
2021-11-15 17:16:17 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Vox Framework'
+copyright = '2021, Jhonatan Teixeira'
+author = 'Jhonatan Teixeira'
 
 release = '0.1'
 version = '0.1.0'
